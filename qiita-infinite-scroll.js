ff34f27e7709993602d0e09e5e3a7c5bf184df3a
hinamiyagk <syobon.hinata.public@gmail.com>
2016-06-11 18:42:10 +0900

    Fix bug that infinite scroll isn't enabled

--- qiita-infinite-scroll.js
+++ qiita-infinite-scroll.js
@@ -1,26 +1,82 @@
 "use strict";
-window.addEventListener("load", () => {
+
+const getActiveFeedPane = () => {
+    return new Promise(resolve => {
+        const activePane = document.querySelector(".feedActivities > .active");
+        if (activePane !== null) {
+            resolve(activePane);
+        } else {
+            const feedActivityWrapper = document.querySelector(".feedActivities");
+            const observer = new MutationObserver(records => {
+                for (let record of records) {
+                    if (record.target.parentNode === feedActivityWrapper && record.target.classList.contains("active")) {
+                        observer.disconnect();
+                        resolve(record.target);
+                        return;
+                    }
+                }
+            });
+            observer.observe(feedActivityWrapper, {
+                childList: false,
+                attributes: true,
+                characterData: false,
+                subtree: true
+            });
+        }
+    });
+};
+
+const getActivity = (activityFeedPane) => {
+    return new Promise(resolve => {
+        if (activityFeedPane.querySelector(".activities") !== null) {
+            resolve(activityFeedPane.querySelector(".activities"));
+            return;
+        }
+        const observer = new MutationObserver(records => {
+            for (let record of records) {
+                if (record.target.classList.contains("activities")) {
+                    observer.disconnect();
+                    resolve(record.target);
+                    return;
+                }
+            }
+        });
+        observer.observe(activityFeedPane, {
+            childList: true,
+            attributes: false,
+            characterData: false,
+            subtree: true
+        });
+    });
+};
+
+const infiniteScroll = activities => {
     let htmlHeight = document.documentElement.scrollHeight;
-    const moreButtons = document.getElementsByClassName("more-button");
-    if (moreButtons[0] === undefined) {
-        return;
-    }
-    const observer = new MutationObserver(mutations => {
+
+    const observer = new MutationObserver(() => {
         htmlHeight = document.documentElement.scrollHeight;
     });
-    observer.observe(document.getElementsByClassName("activities")[0], {
+    observer.observe(activities, {
         childList: true,
         attributes: true,
         characterData: false,
         subtree: true
     });
-    document.addEventListener("scroll", (e) => {
-        const wScroll = e.pageY;
-        if (wScroll + window.innerHeight === htmlHeight) {
-            if (moreButtons[0] === undefined) {
-                return;
-            }
-            moreButtons[0].click();
+
+    document.addEventListener("scroll", e => {
+        if (e.pageY + window.innerHeight === htmlHeight) {
+            const moreButton = activities.parentNode.querySelector(".more-button");
+            if (moreButton === null) { return; }
+            moreButton.click();
         }
     });
-});
+};
+
+const init = () => {
+    getActiveFeedPane().then(activeFeedPane => {
+        return getActivity(activeFeedPane);
+    }).then(activities => {
+        infiniteScroll(activities);
+    });
+};
+document.addEventListener("DOMContentLoaded", init);
