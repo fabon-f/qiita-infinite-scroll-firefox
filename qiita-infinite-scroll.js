0083e41570a838f519019924a7e24dd43f873eba
hinamiyagk <syobon.hinata.public@gmail.com>
2016-12-08 18:03:49 +0900

    Use Disposable and CompositeDisposable for cleanup

--- qiita-infinite-scroll.js
+++ qiita-infinite-scroll.js
@@ -1,12 +1,52 @@
 "use strict";
 
+class Disposable {
+    constructor(disposalAction) {
+        this.disposed = false;
+        this.disposalAction = disposalAction;
+    }
+    dispose() {
+        if (!this.disposed) {
+            this.disposalAction();
+            this.disposalAction = null;
+            this.disposed = true;
+        }
+    }
+}
+
+class CompositeDisposable {
+    constructor() {
+        this.disposables = new Set();
+        this.disposed = false;
+    }
+    add(disposable) {
+        this.disposables.add(disposable);
+    }
+    remove(disposable) {
+        this.disposables.delete(disposable);
+    }
+    dispose() {
+        if (!this.disposed) {
+            this.disposables.forEach(disposable => disposable.dispose());
+            this.disposables = null;
+            this.disposed = true;
+        }
+    }
+}
+
+const disposables = new CompositeDisposable();
+
 const getActivities = wrapper => {
     return new Promise(resolve => {
+        const disposable = new Disposable(() => observer.disconnect());
+        disposables.add(disposable);
+
         const observer = new MutationObserver(mutations => {
             for (let mutation of mutations) {
                 for (let addedNode of mutation.addedNodes) {
                     if (addedNode.classList && addedNode.classList.contains("activities")) {
-                        observer.disconnect();
+                        disposable.dispose();
+                        disposables.remove(disposable);
                         resolve(addedNode);
                         return;
                     }
@@ -30,13 +70,23 @@ const infiniteScroll = activities => {
         subtree: true
     });
 
-    document.addEventListener("scroll", e => {
+    const observerDisposable = new Disposable(() => observer.disconnect());
+    disposables.add(observerDisposable);
+
+    const scrollCallback = e => {
         if (e.pageY + window.innerHeight === htmlHeight) {
             const moreButton = activities.parentNode.querySelector(".more-button");
             if (moreButton === null) { return; }
             moreButton.click();
         }
-    }, { passive: true });
+    };
+
+    const scrollListenerDisposable = new Disposable(() => {
+        document.removeEventListener("scroll", scrollCallback);
+    });
+    disposables.add(scrollListenerDisposable);
+
+    document.addEventListener("scroll", scrollCallback, { passive: true });
 };
 
 const init = () => {
